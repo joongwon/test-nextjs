cf3ac395de894dcba3feb1a471b179ef560de32f
joongwon <winterdawnlight@snu.ac.kr>
2022-02-16 15:42:17 +0900

    add blog

---
+++ components/date.js
@@ -0,0 +1,8 @@
+import { format, parseISO } from "date-fns";
+
+const Date = ({ dateString }) => (
+  <time dateTime={dateString}>
+    {format(parseISO(dateString), "LLLL d, yyyy")}
+  </time>
+);
+export default Date;
---
+++ pages/posts/[id].js
@@ -0,0 +1,33 @@
+import Layout from "../../components/layout";
+import { getAllPostIds, getPostData } from "../../lib/posts";
+import Head from "next/head";
+import Date from "../../components/date";
+import utilStyles from "../../styles/utils.module.css";
+
+export const getStaticPaths = async () => ({
+  paths: getAllPostIds(),
+  fallback: false,
+});
+
+export const getStaticProps = async ({ params }) => ({
+  props: {
+    postData: await getPostData(params.id),
+  },
+});
+
+export default function Post({ postData }) {
+  return (
+    <Layout>
+      <Head>
+        <title>{postData.title}</title>
+      </Head>
+      <article>
+        <h1 className={utilStyles.headingXl}>{postData.title}</h1>
+        <div className={utilStyles.lightText}>
+          <Date dateString={postData.date} />
+        </div>
+        <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+      </article>
+    </Layout>
+  );
+}
